928b31223a7ba02bfafdb5fe29e260f89f293007
shikto1 <islamahidul1@gmail.com>
2017-08-31 10:35:21 +0600

    //Commit by Shishir......................

--- app/src/main/java/com/example/shishir/blood/ExtraClass/MyApplication.java
+++ app/src/main/java/com/example/shishir/blood/ExtraClass/MyApplication.java
@@ -1,38 +1,38 @@
-//package com.example.shishir.blood.ExtraClass;
-//
-//import android.app.Application;
-//import android.content.Intent;
-//
-//import com.example.shishir.blood.Activity.NotificationActivity;
-////import com.onesignal.OSNotificationOpenResult;
-//import com.onesignal.OneSignal;
-//
-//import org.json.JSONException;
-//import org.json.JSONObject;
-//
-///**
-// * Created by Shishir on 8/10/2017.
-// */
-//
-//public class MyApplication extends Application {
-//
-//    @Override
-//    public void onCreate() {
-//        super.onCreate();
-//        OneSignal.startInit(this).setNotificationOpenedHandler(new OneSignal.NotificationOpenedHandler() {
-//            @Override
-//            public void notificationOpened(OSNotificationOpenResult result) {
-//                JSONObject data=result.notification.payload.additionalData;
-//
-//                Intent intent=new Intent(getApplicationContext(),NotificationActivity.class);
-//                intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT | Intent.FLAG_ACTIVITY_NEW_TASK);
-//                try {
-//                    intent.putExtra("dataM",data.getString("dataM"));
-//                }catch (JSONException e){
-//                    e.printStackTrace();
-//                }startActivity(intent);
-//            }
-//        }).autoPromptLocation(true).init();
-//
-//    }
-//}
+package com.example.shishir.blood.ExtraClass;
+
+import android.app.Application;
+import android.content.Intent;
+
+import com.example.shishir.blood.Activity.NotificationActivity;
+import com.onesignal.OSNotificationOpenResult;
+import com.onesignal.OneSignal;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by Shishir on 8/10/2017.
+ */
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        OneSignal.startInit(this).setNotificationOpenedHandler(new OneSignal.NotificationOpenedHandler() {
+            @Override
+            public void notificationOpened(OSNotificationOpenResult result) {
+                JSONObject data=result.notification.payload.additionalData;
+
+                Intent intent=new Intent(getApplicationContext(),NotificationActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT | Intent.FLAG_ACTIVITY_NEW_TASK);
+                try {
+                    intent.putExtra("dataM",data.getString("dataM"));
+                }catch (JSONException e){
+                    e.printStackTrace();
+                }startActivity(intent);
+            }
+        }).autoPromptLocation(true).init();
+
+    }
+}
