05d9bd0746b77515d61d5add6f588591bd27c914
shikto1 <islamahidul1@gmail.com>
2017-09-05 21:21:23 +0600

    //Shishir

--- app/src/main/java/com/example/shishir/blood/Activity/RegisterLoginActivity.java
+++ app/src/main/java/com/example/shishir/blood/Activity/RegisterLoginActivity.java
@@ -1,15 +1,23 @@
 package com.example.shishir.blood.Activity;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.shishir.blood.CreateAccountUsingGmail;
+import com.example.shishir.blood.Fragment.LoginWithFacebookOrMailFragment;
 import com.example.shishir.blood.R;
 
 public class RegisterLoginActivity extends AppCompatActivity {
+
+    FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_login);
+
+        fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().add(R.id.parentLayoutRegisterLogin, new LoginWithFacebookOrMailFragment()).commit();
     }
 }
