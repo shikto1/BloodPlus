aa69ac4193e084356c520ac0cf785b8e7fc9d30f
shikto1 <islamahidul1@gmail.com>
2017-09-05 13:24:48 +0600

    //shishir

---
+++ app/src/main/java/com/example/shishir/blood/CreateAccountUsingGmail.java
@@ -0,0 +1,20 @@
+package com.example.shishir.blood;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class CreateAccountUsingGmail extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_accouot_using_gmail, container, false);
+    }
+
+}
