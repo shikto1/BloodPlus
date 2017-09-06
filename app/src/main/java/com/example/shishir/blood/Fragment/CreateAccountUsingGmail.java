15ea158c93f5556fdef6e5e431ec1ef86818f036
shikto1 <islamahidul1@gmail.com>
2017-09-06 21:37:10 +0600

    //shishir

--- app/src/main/java/com/example/shishir/blood/Fragment/CreateAccountUsingGmail.java
+++ app/src/main/java/com/example/shishir/blood/Fragment/CreateAccountUsingGmail.java
@@ -3,8 +3,10 @@ package com.example.shishir.blood.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,6 +24,13 @@ import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -31,6 +40,8 @@ public class CreateAccountUsingGmail extends Fragment implements View.OnClickLis
 
     FragmentManager fragmentManager;
     Button registerWIthEmail, loginHereBtnAtFBG;
+
+    FirebaseAuth auth;
 
     TextView loginStatus;
 
@@ -53,6 +64,7 @@ public class CreateAccountUsingGmail extends Fragment implements View.OnClickLis
         loginHereBtnAtFBG = (Button) view.findViewById(R.id.loginHereFbEmail);
         loginStatus = (TextView) view.findViewById(R.id.loginStatus);
         fragmentManager = getActivity().getSupportFragmentManager();
+        auth = FirebaseAuth.getInstance();
 
 
         registerWIthEmail.setOnClickListener(this);
@@ -69,7 +81,7 @@ public class CreateAccountUsingGmail extends Fragment implements View.OnClickLis
             @Override
             public void onSuccess(LoginResult loginResult) {
 
-                handleFacebookAccessToken(loginResult.getAccessToken());
+                createAccountUsingFb(loginResult.getAccessToken());
 
             }
 
@@ -86,6 +98,27 @@ public class CreateAccountUsingGmail extends Fragment implements View.OnClickLis
         });
 
 
+    }
+
+    private void createAccountUsingFb(AccessToken accessToken) {
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(accessToken.getToken());
+        auth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (!task.isSuccessful()) {
+                    ToastMessage("Authentication Failed");
+                } else {
+                    FirebaseUser user = task.getResult().getUser();
+                    Bundle b=new Bundle();
+                    b.putString("uid",user.getUid());
+                    b.putString("name",user.getDisplayName());
+                    b.putString("email",user.getEmail());
+                    b.putString("choice","fb");
+
+                }
+            }
+        });
     }
 
     private void handleFacebookAccessToken(AccessToken accessToken) {
